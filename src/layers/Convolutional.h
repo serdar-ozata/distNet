e9dc3e3c268ebae4d070f7b6e94249de64dea695
serdar-ozata <serdar1901@gmail.com>
2024-11-03 22:10:50 +0300

    conv WIP

--- src/layers/Convolutional.h
+++ src/layers/Convolutional.h
@@ -6,9 +6,50 @@
 #define DISTNET_CONVOLUTIONAL_H
 
 
+#include <cstdio>
 #include "Layer.h"
-// todo
+#include "mpi.h"
+
+typedef enum {
+    ZERO,
+    NONE,
+    SAME,
+} PaddingType;
+
+
 class Convolutional : public Layer {
+private:
+    int stride;
+    PaddingType padding_type;
+    Matrix *kernel;
+    Matrix *bias;
+    Matrix *m_kernel;
+    Matrix *m_bias;
+    Matrix *v_kernel;
+    Matrix *v_bias;
+    Matrix *kernel_grad;
+    Matrix *bias_grad;
+    double beta1 = 0.9;
+    double beta2 = 0.999;
+    double epsilon = 1e-8;
+    double lr = 0.001;
+public:
+    Convolutional(Matrix *input, int kernel_size, int stride, PaddingType padding_type, unsigned int seed) : Layer(input) {
+        this->stride = stride;
+        this->padding_type = padding_type;
+        if (kernel_size % 2 == 0 || kernel_size < 1) {
+            fprintf(stderr, "Invalid kernel size\n");
+        }
+        if (stride < 1) {
+            fprintf(stderr, "Invalid stride\n");
+        }
+        int output_size;
+        if (padding_type == NONE) {
+            output_size = (input->m - kernel_size + 1) / stride;
+        } else {
+            output_size = input->m / stride;
+        }
+    }
 
 };
 
--- src/layers/Dropout.h
+++ src/layers/Dropout.h
@@ -31,10 +31,10 @@ public:
         }
     }
 
-    Matrix * backward(Matrix *error, int step) override {
+    Matrix* backward(Matrix *error, int step) override {
         for (int i = 0; i < input->m; i++) {
             for (int j = 0; j < input->n; j++) {
-                error->data[i][j] = mask[i * input->n + j] * error->data[i][j];
+                error->data[i][j] *= mask[i * input->n + j];
             }
         }
         return error;
